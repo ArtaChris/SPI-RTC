161f5b2e00da24438e9e9568263d480a59ba36d9
Chris A <45605287+ArtaChris@users.noreply.github.com>
2021-03-28 22:11:23 -0700

    Read time init func

--- circuitpython/ds3234.py
+++ circuitpython/ds3234.py
@@ -1,5 +1,6 @@
 from micropython import const
 from adafruit_bus_device.spi_device import SPIDevice
+import time
 
 # _________________________________________________________________
 # |                         Address Bits                          |
@@ -26,12 +27,12 @@ _DS3234_YEAR = const(0x06) # BCD
 
 _DS3234_ALARM1_SECONDS = const(0x07) # BCD
 _DS3234_ALARM1_MINUTE = const(0x08) # BCD
-_DS3234_ALARM2_HOUR = const(0x09) # BCD
+_DS3234_ALARM1_HOUR = const(0x09) # BCD
 _DS3234_ALARM1_DAY = const(0x0A) # BCD
 
-_DS3234_ALARM2_MINUTE = const(0x0B)
-_DS3234_ALARM2_HOUR = const(0x0C)
-_DS3234_ALARM2_DAY = const(0x0D)
+_DS3234_ALARM2_MINUTE = const(0x0B) # BCD
+_DS3234_ALARM2_HOUR = const(0x0C) # BCD
+_DS3234_ALARM2_DAY = const(0x0D) # BCD
 
 _DS3234_CONTROL_REG = const(0x0E)
 _DS3234_STATUS_REG = const(0x0F)
@@ -49,5 +50,40 @@ _DS3234_SRAM_DATA = const(0x19)
 _DS3234_MAX_SCLK = const(4000000)
 
 class DS3234:
-    def __init__(self, spi, cs)
+    time_buf = bytearray(8)
+    alarm1_buf = bytearray(4)
+    alarm2_buf = bytearray(3)
+
+    address_buf = bytearray(1)
+
+    def __init__(self, spi, cs):
         self.spi_device = SPIDevice(spi, cs, baudrate=_DS3234_MAX_SCLK, polarity=0, phase=1)
+
+
+    def getTime(self):
+        buf = bytearray(8)
+        buf[0] = _DS3234_SECONDS & _DS3234_READ_MASK
+        t = list(self.time_buf)
+        with self.spi_device as spi:
+            spi.write_readinto(buf, self.time_buf)
+        t[7] = (self.time_buf[7] & 0x0F) + (self.time_buf[7] >> 4) * 10 + 2000
+        t[6] = (self.time_buf[6] & 0x0F) + ((self.time_buf[6] & 0x10) >> 4) * 10
+        t[5] = (self.time_buf[5] & 0x0F) + ((self.time_buf[5] & 0x30) >> 4)
+        t[4] = (self.time_buf[4] & 0x07)
+        t[3] = (self.time_buf[3] & 0x0F) + ((self.time_buf[3] & 0x10) >> 4) * 10
+        t[2] = (self.time_buf[2] & 0x0F) + ((self.time_buf[2] & 0x70) >> 4) * 10
+        t[1] = (self.time_buf[1] & 0x0F) + ((self.time_buf[1] & 0x70) >> 4) * 10
+        return time.struct_time(
+            (
+                t[7],
+                t[6],
+                t[5],
+                t[3],
+                t[2],
+                t[1],
+                t[4],
+                -1,
+                -1,
+            )
+        )
+
